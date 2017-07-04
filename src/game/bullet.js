80f11edda3e5c5a604d7991bec29cd9baa8e06c0
Ciaran Broderick <cb@cix.ie>
2017-07-04 13:24:57 +0100

    Working on #4

---
+++ src/game/bullet.js
@@ -0,0 +1,30 @@
+const bulletSize = 5;
+const bulletSpeed = 25;
+const maxBounces = 3;
+const maxDamage = 10;
+
+class Bullet {
+	constructor(x, y, angle, owner, bulletNum) {
+		// x and y will come in as the center point
+		this.x = x - (bulletSize / 2);
+		this.y = y - (bulletSize / 2);
+		this.xChange = bulletSpeed * Math.cos(angle);
+		this.yChange = -bulletSpeed * Math.sin(angle);
+		this.bouncesRemaining = maxBounces;
+		this.owner = owner;
+		this.bulletNum = bulletNum;
+	}
+
+	draw /* istanbul ignore next */ (context) {
+		context.fillStyle = 'white';
+		context.fillRect(this.x, this.y, bulletSize, bulletSize);
+		this.updatePosition();
+	}
+
+	updatePosition() {
+		this.x += this.xChange;
+		this.y += this.yChange;
+	}
+}
+
+exports.Bullet = Bullet;
