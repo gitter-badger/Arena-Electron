1f9196bab79237fada76b5c8939a9e6a9dabd58c
Ciaran Broderick <cb@cix.ie>
2017-07-04 14:40:25 +0100

    Added tests for new methods in player that handle shooting

--- test/test_player.js
+++ test/test_player.js
@@ -134,6 +134,58 @@ describe("Player", () => {
         });
     });
 
+    describe('shooting', () => {
+        it('will shoot a bullet', () => {
+            player.shoot({pageX: 245, pageY: 155}).should.equal(true);
+            player.bullets[0].should.not.equal(null);
+            player.currentBullets.should.equal(2);
+        });
+
+        it('will not shoot more than 3 bullets', () => {
+            player.shoot({pageX: 245, pageY: 155}).should.equal(true);
+            player.shoot({pageX: 245, pageY: 155}).should.equal(true);
+            player.shoot({pageX: 245, pageY: 155}).should.equal(true);
+            player.shoot({pageX: 245, pageY: 155}).should.equal(false);
+            player.bullets.forEach((b) => {
+                b.should.not.equal(null);
+            });
+        });
+    });
+
+    describe('getAngle', () => {
+        let angle;
+
+        beforeEach(() => {
+            // Testing a 45 degree angle
+            angle = Math.PI / 4;
+        });
+
+        describe('returns the value', () => {
+            it('+x -y', () => {
+                // +x -y adds 0 to the angle
+                player.getAngle(0, 0, 45, -45).should.equal(angle);
+            });
+
+            it('+x +y', () => {
+                // +x +y adds 3PI / 2 to the angle
+                angle += (3 * Math.PI) / 2;
+                player.getAngle(0, 0, 45, 45).should.equal(angle);
+            });
+
+            it('-x -y', () => {
+                // -x -y adds PI / 2 to the angle
+                angle += Math.PI / 2;
+                player.getAngle(0, 0, -45, -45).should.equal(angle);
+            });
+
+            it('-x +y', () => {
+                // -x +y adds 3I to the angle
+                angle += Math.PI;
+                player.getAngle(0, 0, -45, 45).should.equal(angle);
+            });
+        });
+    });
+
     describe("movement", () => {
         let playerSpeed = 4;
         describe("can move", () => {
