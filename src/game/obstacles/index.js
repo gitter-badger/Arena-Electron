9a5f07af7d9c2cd4cfde0d82522656fb0ac0ff20
Ciaran Broderick <broderickciaran@gmail.com>
2017-09-08 01:33:43 +0100

    Creating empty gh-pages branch

--- src/game/obstacles/index.js
+++
@@ -1,9 +0,0 @@
-const BulletBlock = require('./bulletBlock').BulletBlock;
-const PlayerBlock = require('./playerBlock').PlayerBlock;
-const AllBlock = require('./allBlock').AllBlock;
-const DamageBlock = require('./damageBlock').DamageBlock;
-
-exports.BulletBlock = BulletBlock;
-exports.PlayerBlock = PlayerBlock;
-exports.AllBlock = AllBlock;
-exports.DamageBlock = DamageBlock;
