3f6a7c24c8e9c69aac5b8e1540bac9af22731463
Ciaran Broderick <broderickciaran@gmail.com>
2017-09-13 16:19:32 +0100

    Take in instance of server in lobby constructor

--- src/server/lobby.js
+++ src/server/lobby.js
@@ -1,11 +1,10 @@
 // File for handling messages while in the lobby state
 
 class Lobby {
-    constructor(players, sockets) {
+    constructor(server) {
         // Takes in arrays of usernames and sockets
         // Eventually, the players array will be an array of Player objects which will contain their socket
-        this.players = players;
-        this.sockets = sockets;
+        this.server = server;
     }
 
     handle(message, socket) {
@@ -23,22 +22,22 @@ class Lobby {
         // Remove the player from the list and send the new lobby status to the players in the lobby
         // Find the player who left
         let i;
-        for (i = 0; i < this.sockets.length; i ++) {
-            if (this.sockets[i] === socket) {
+        for (i = 0; i < this.server.sockets.length; i ++) {
+            if (this.server.sockets[i] === socket) {
                 break;
             }
         }
-        process.send(this.players[i] + ' has left the lobby');
-        this.players[i] = null;
+        this.server.playerObjects[i] = null;
         socket.close();
-        this.sockets[i] = null;
+        this.server.sockets[i] = null;
+        this.server.numPlayers --;
 
         let data = {
             command: 'LOBBY',
-            data: this.players
+            data: this.server.playerObjects // TODO - Replace with Player Objects once implemented
         };
         data = JSON.stringify(data);
-        this.sockets.forEach((sock) => {
+        this.server.sockets.forEach((sock) => {
             if (sock !== null) {
                 sock.sendUTF(data);
             }
